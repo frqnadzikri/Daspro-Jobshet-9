985817d88660569539e5408739444d818469d0f9
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-11-06 22:14:35 +0700

    Modifikasi Kode No.3

--- ArrayBilangan15.java
+++ ArrayBilangan15.java
@@ -1,9 +1,11 @@
-public class ArrayBilangan15 {
+public class ArrayBilangan15 { 
     public static void main(String[] args) {
-        int[] bil = {5, 13, -7, 17};  // inisialisasi langsung
+
+        int[] bil = {5, 13, -7, 17};
 
         for (int i = 0; i < 4; i++) {
             System.out.println(bil[i]);
         }
     }
 }
+
