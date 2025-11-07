25b9fd239487522427323d63b45f4822e1d6b5a5
Furqon Adzikri <furqonadzikri311@gmail.com>
2025-11-07 21:54:03 +0700

    Modifikasi Percobaan 3 No.1

--- ArrayRataNilai15.java
+++ ArrayRataNilai15.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
-public class ArrayRataNilai15 { 
+public class ArrayRataNilai15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int[] nilaiMhs = new int[10]; 
+
+        int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int lulus = 0; // untuk menghitung jumlah mahasiswa yang nilainya > 70
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
@@ -15,11 +16,15 @@ public class ArrayRataNilai15 {
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                lulus++;
+            }
         }
 
         rata2 = total / nilaiMhs.length;
 
         System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Jumlah mahasiswa yang lulus (nilai > 70) = " + lulus);
 
         sc.close();
     }
